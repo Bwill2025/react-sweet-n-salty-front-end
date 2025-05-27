e257935f8ed8e96eb493b53b5bb5c38fb1b17151
Victoria Tillero <victoriatillero6@gmail.com>
2025-05-27 13:24:11 -0400

    fixed edit/delete buttons to only show for snack owner

--- src/components/SnackDetail/SnackDetail.jsx
+++ src/components/SnackDetail/SnackDetail.jsx
@@ -1,27 +1,35 @@
+import { useContext } from 'react';
+import { UserContext } from '../../contexts/UserContext';
+
 const SnackDetail = (props) => {
+  const { user } = useContext(UserContext);
 
-    if (!props.selected) {
-        return null;
+  if (!props.selected) return null;
 
-    }
-    return (
-        <div>
+  const isOwner = user?._id === props.selected.createdBy;
+
+  return (
+    <div>
       <h1>{props.selected.name}</h1>
       <h2>Description: {props.selected.description}</h2>
       <h2>Emoji: {props.selected.emoji}</h2>
-      <div>
-        <button onClick={() => props.handleFormView(props.selected)}>
-          Edit Snack
-        </button>
-        <button onClick={() => props.handleDeleteSnack(props.selected._id)}>
-          Delete Snack
-        </button>
-        <button onClick={() => props.handleSelect(null)}>
-          Close Details
-        </button>
-      </div>
+
+      {isOwner && ( 
+        <div>
+          <button onClick={() => props.handleFormView(props.selected)}>
+            Edit Snack
+          </button>
+          <button onClick={() => props.handleDeleteSnack(props.selected._id)}>
+            Delete Snack
+          </button>
+        </div>
+      )}
+
+      <button onClick={() => props.handleSelect(null)}>
+        Close Details
+      </button>
     </div>
   );
 };
 
-  export default SnackDetail;
+export default SnackDetail;
